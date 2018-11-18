74c0c14eecf2fd283bed9628ab2d24df2212309f
woranitha <gracenuad@gmail.com>
2018-11-18 12:30:17 +0700

    ready1

--- app/src/main/java/com/example/android/openrandom/MainActivity.java
+++ app/src/main/java/com/example/android/openrandom/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.openrandom;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -12,11 +13,35 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button button;
     Random r;
-    Integer[] images = {R.drawable.bachor};
+    Integer[] images = {R.drawable.bachor, R.drawable.bucket, R.drawable.burin, R.drawable.chick, R.drawable.eight4, R.drawable.eimaim
+            , R.drawable.goin, R.drawable.mahalai, R.drawable.mrgrill, R.drawable.shabu2n, R.drawable.pela
+            , R.drawable.steaktechno, R.drawable.thinkmilk, R.drawable.timber};
+    int pickedImage, lastPicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageRand);
+        button = (Button) findViewById(R.id.butRand);
+        r = new Random();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                do{
+                    pickedImage = r.nextInt(images.length);
+                } while(pickedImage == lastPicked);
+
+                lastPicked = pickedImage;
+                imageView.setImageResource(images[r.nextInt(pickedImage)]);
+            }
+        });
+
     }
 }
+
+
+
+
